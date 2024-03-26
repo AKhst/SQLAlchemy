63dee1b9ec2a079dd7e06bc906872746394b05ea
Your Name <you@example.com>
2024-03-27 00:47:46 +0300

    Create Model employee and clients
    Signed-off-by: Your Name <you@example.com>

--- database.py
+++ database.py
@@ -1,55 +1,19 @@
 # This is a sample Python script.
-from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from models import engine
+from models import ClientsOrm  # Импортируем модель из файла models.py
 
-from sqlalchemy import String
-from sqlalchemy import URL, create_engine, text
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+# Создаем фабрику сеансов
+Session = sessionmaker(bind=engine)
 
-from config import connection_string
+# Создаем объект сеанса
+session = Session()
 
-engine = create_engine(
-    connection_string,
-    echo=True
-    # pool_size=5,
-    # max_overflow=10
-)
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    print(f"{res=}")
+# Создаем объект клиента
+clients = ClientsOrm(fullname="Odelinda Grzelewski", adress="7 Everett Park", phone="363-492-2952")
 
+# Добавляем объект в сеанс
+session.add(clients)
 
-# declarative base class
-class Base(DeclarativeBase):
-    pass
-
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-class ClientsOrm(Base):
-    __tablename__ = "clients"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    fullname: Mapped[str]
-    adress: Mapped[str] = mapped_column(String(30))
-    phone: Mapped[str]
-
-
-class EmployeeOrm(Base):
-    __tablename__ = "employee"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    lastname: Mapped[str]
-    firstname: Mapped[str]
-    middlename: Mapped[str]
-    jobtitle: Mapped[str]
-    address: Mapped[str]
-    homephone: Mapped[int]
-    dateofbirth: Mapped[datetime]
-    adress: Mapped[str] = mapped_column(String(50))
-    phone: Mapped[str]
-
-
-Base.metadata.create_all(engine)
+# Фиксируем изменения в базе данных
+session.commit()
